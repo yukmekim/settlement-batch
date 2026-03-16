66af6bd9b33f713f6621019303751c51847aa466
yukmekim <gyjj1243@gmail.com>
2026-03-16 20:18:03 +0900

    feat: Spring Boot 초기 환경 구성

---
+++ src/main/java/dev/yukmekim/settlement/SettlementBatchApplication.java
@@ -0,0 +1,13 @@
+package dev.yukmekim.settlement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SettlementBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SettlementBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/yukmekim/settlement/SettlementBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.yukmekim.settlement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SettlementBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
